bb04ca0c0dd07c2645e74a5876730af3a8a41422
Raju9347 <chinnaraju0505@gmail.com>
2025-12-10 17:34:36 +0530

    updated Application.java file

--- src/main/java/com/example/app/Application.java
+++ src/main/java/com/example/app/Application.java
@@ -7,17 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * Main application entrypoint.
  */
 @SpringBootApplication
-public final class Application {
-
+public class Application {
     private Application() {
         // prevent instantiation
     }
 
-    /**
-     * Starts the Spring Boot application.
-     * @param args CLI args
-     */
     public static void main(final String[] args) {
         SpringApplication.run(Application.class, args);
     }
 }
+
